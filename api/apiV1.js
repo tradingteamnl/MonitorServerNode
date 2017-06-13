47e2b5125e25d3dce7ab5d601fc4a25bcb15678d
michel <micheldaniel2@hotmail.com>
2017-06-14 00:20:04 +0200

    key module
    key script gemaakt

--- api/apiV1.js
+++ api/apiV1.js
@@ -11,6 +11,18 @@ var fileLocatie = fs.readFileSync("./temp/installerLocatie.txt");
 
 //laat extra modules
 var consoleBericht = require(fileLocatie+'/ConsoleColor.js');
+var getKey = require(fileLocatie+'/scripts/GetKey.js');
+
+//get key
+router.get('/getKey/:macAddress', function(req, res){
+    
+    //krijg de key
+    
+    //zet de key in het database
+    
+    //stuur de key terug
+    res.send();
+});
 
 //licentieCheck router
 router.get('/licentie/:licentie', function(req, res){
@@ -18,8 +30,6 @@ router.get('/licentie/:licentie', function(req, res){
     //vraag data op uit de parameters
     var licentie = req.params('licentie');
 });
-
-
 
 
 
